9d05cc403cdd1bfeb3f34cfe77e78b8c4e5af800
CrayolaXans <crayolaxans@gmail.com>
2025-01-08 17:44:14 -0500

    links

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WTR-F24",
+  base: "/WTR-F24/home",
   server: {
     port:3000,
   },
