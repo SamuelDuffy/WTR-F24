fe0772a2e1bd4d934b13c34bfca32b5764716316
CrayolaXans <crayolaxans@gmail.com>
2025-01-08 17:50:53 -0500

    links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <NavLink to="/WTR-F24/home" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Home</NavLink>
+      <NavLink to="/WTR-F24/" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Home</NavLink>
       <NavLink to="/WTR-F24/experience" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Experience</NavLink>
       <NavLink to="/WTR-F24/side-quests" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Side Quests</NavLink>
     </nav>
